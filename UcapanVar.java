b6e7f4a8898ca97c47083eb8c58243687ac02bd1
Alfisha Agnina Hizni <alfishanina@gmail.com>
2024-09-21 21:00:48 +0800

    Add files via upload

---
+++ UcapanVar.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author user
+ */
+public class UcapanVar {
+
+    public static void main(String[] args) {
+        String universitas = "Halo ULM";
+        String berdaya = "ULM Berdaya Saing dan Unggul di Bidang Lahan Basah";
+        String Ilkom = "Ilkom ULM dengan Teknologi Kekinian";
+
+        System.out.println(universitas);
+        System.out.println(berdaya);
+        System.out.println(Ilkom);
+        System.out.println("------");
+        System.out.println(berdaya + ", " + Ilkom);
+    }
+}
